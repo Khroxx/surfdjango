f930cc4e30ad4cb7883fc3f1550299ba0bf225bb
Khroxx <134767175+Khroxx@users.noreply.github.com>
2024-10-28 10:23:41 +0100

    added update and delete

--- surfdjango/corporations/urls.py
+++ surfdjango/corporations/urls.py
@@ -7,4 +7,6 @@ from .views import delete_corp_view
 app_name = 'corporations'
 urlpatterns = [
     path('create/', create_corp_view, name="create-corp"),
+    path('<int:pk>/update/', update_corp_view, name="update-corp"),
+    path('<int:pk>/delete/', delete_corp_view, name="delete-corp")
 ]
--- surfdjango/corporations/views.py
+++ surfdjango/corporations/views.py
@@ -14,6 +14,28 @@ class CorporationCreateView(SuccessMessageMixin, CreateView):
     template_name = "corporations/corporations_create.html"
     success_message = _("Corporation successfully created")
 
+    def get_success_url(self):
+        return reverse_lazy("corporations:list")
+
+
+create_corp_view = CorporationCreateView.as_view()
+
+
+class CorportaionUpdateView(SuccessMessageMixin, UpdateView):
+    model = Corporation
+    fields = ["name", "address", "url", "user"]
+    template_name = "corporations/corporations_update.html"
+    success_message = _("Corporation successfully updated")
+
+    def get_success_url(self):
+        return reverse_lazy("corporations:list")
+
+
+update_corp_view = CorportaionUpdateView.as_view()
+
+
+class CorporationDeleteView(SuccessMessageMixin, DeleteView):
+    model = Corporation
     fields = ["name", "address", "url", "user"]
     template_name = "corporations/corporations_delete.html"
     success_message = _("Corporation successfully updated")
