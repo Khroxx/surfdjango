7fa03f1ae9a5dcd6289ca4e206378c77166574ef
Khroxx <134767175+Khroxx@users.noreply.github.com>
2024-10-28 18:40:51 +0100

    Update tests.py

--- surfdjango/corporations/tests.py
+++ surfdjango/corporations/tests.py
@@ -22,17 +22,17 @@ class CorporationToUserTest(TestCase):
         )
 
 
-def test_corp_user_created(self):
-    self.assertEqual(self.corporation.name, "Test Corp")  # noqa: PT009
-    self.assertEqual(self.corporation.address, "Test Address 123")  # noqa: PT009
-    self.assertEqual(self.corporation.url, "https://testdomain.com")  # noqa: PT009
-    self.assertEqual(self.corporation.user, self.user)  # noqa: PT009
+    def test_corp_user_created(self):
+        self.assertEqual(self.corporation.name, "Test Corp")  # noqa: PT009
+        self.assertEqual(self.corporation.address, "Test Address 123")  # noqa: PT009
+        self.assertEqual(self.corporation.url, "https://testdomain.com")  # noqa: PT009
+        self.assertEqual(self.corporation.user, self.user)  # noqa: PT009
 
 
-def test_user_created(self):
-    self.assertEqual(self.user.username, "testuser")  # noqa: PT009
+    def test_user_created(self):
+        self.assertEqual(self.user.username, "testuser")  # noqa: PT009
 
 
-def test_user_corp_relationship(self):
-    self.assertEqual(self.user.corporations.count(), 1)  # noqa: PT009
-    self.assertEqual(self.user.corporations.first(), self.corporation)  # noqa: PT009
+    def test_user_corp_relationship(self):
+        self.assertEqual(self.user.corporations.count(), 1)  # noqa: PT009
+        self.assertEqual(self.user.corporations.first(), self.corporation)  # noqa: PT009
