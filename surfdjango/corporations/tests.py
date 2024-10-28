a26569fff7622f729f6c05556e9e0a11ca59566c
Khroxx <134767175+Khroxx@users.noreply.github.com>
2024-10-28 18:42:11 +0100

    Update tests.py

--- surfdjango/corporations/tests.py
+++ surfdjango/corporations/tests.py
@@ -21,17 +21,14 @@ class CorporationToUserTest(TestCase):
             user=self.user,
         )
 
-
     def test_corp_user_created(self):
         self.assertEqual(self.corporation.name, "Test Corp")  # noqa: PT009
         self.assertEqual(self.corporation.address, "Test Address 123")  # noqa: PT009
         self.assertEqual(self.corporation.url, "https://testdomain.com")  # noqa: PT009
         self.assertEqual(self.corporation.user, self.user)  # noqa: PT009
 
-
     def test_user_created(self):
         self.assertEqual(self.user.username, "testuser")  # noqa: PT009
-
 
     def test_user_corp_relationship(self):
         self.assertEqual(self.user.corporations.count(), 1)  # noqa: PT009
